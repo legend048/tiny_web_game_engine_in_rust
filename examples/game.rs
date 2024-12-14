6a4f6f12e5f8a9ba9ce2dfae36a712905a906ede
legend048 <pklegend048@gmail.com>
2024-12-15 02:02:45 +0530

    change bg color with key presses

--- examples/game.rs
+++ examples/game.rs
@@ -1,10 +1,18 @@
 fn main() {
-    let mut blue_amount = 0.0;
+    // let mut blue_amount = 0.0;
 
-    game_engine::set_event_handler(move ||{
-        blue_amount += 0.1;
-        game_engine::clear_screen_color(0.0, 0.0, blue_amount, 1.0);
-    });
+    // game_engine::set_event_handler(move |_key|{
+    //     blue_amount += 0.1;
+    //     game_engine::clear_screen_color(0.0, 0.0, blue_amount, 1.0);
+    // });
 
     // game_engine::clear_screen_color(0.0, 0.0, 1.0, 1.0);
+
+    game_engine::set_event_handler(move |key| match key {
+        game_engine::Key::Left => game_engine::clear_screen_color(1.0, 0.0, 0.0, 1.0),
+        game_engine::Key::Right => game_engine::clear_screen_color(0.0, 1.0, 0.0, 1.0),
+        game_engine::Key::Up => game_engine::clear_screen_color(0.0, 0.0, 1.0, 1.0),
+        game_engine::Key::Down => game_engine::clear_screen_color(0.0, 1.0, 1.0, 1.0),
+        game_engine::Key::Space => game_engine::clear_screen_color(1.0, 1.0, 0.0, 1.0),
+    });
 }
