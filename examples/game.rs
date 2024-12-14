8c6521a6dc5d2324455d53fcde85412d80d43ab4
legend048 <pklegend048@gmail.com>
2024-12-15 01:44:00 +0530

    blue colour change on keydown

--- examples/game.rs
+++ examples/game.rs
@@ -1,3 +1,10 @@
 fn main() {
-    game_engine::clear_screen_color(0.0, 0.0, 1.0, 1.0);
+    let mut blue_amount = 0.0;
+
+    game_engine::set_event_handler(move ||{
+        blue_amount += 0.1;
+        game_engine::clear_screen_color(0.0, 0.0, blue_amount, 1.0);
+    });
+
+    // game_engine::clear_screen_color(0.0, 0.0, 1.0, 1.0);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,19 @@ pub fn clear_screen_color(red: f32, green: f32, blue: f32, alpha: f32){
     }
 }
 
+thread_local! {
+    pub static EVENT_HANDLER: std::cell::RefCell<Box<dyn FnMut()>> = std::cell::RefCell::new(Box::new(||{}));
+}
+
+pub fn set_event_handler(function: impl FnMut() + 'static) {
+    EVENT_HANDLER.with(|event_handler| {
+        *event_handler.borrow_mut() = Box::new(function);
+    });
+}
+
 #[no_mangle]
 pub extern "C" fn key_pressed() {
-    clear_screen_color(0.0, 1.0, 0.0, 1.0);
+    // clear_screen_color(0.0, 1.0, 0.0, 1.0);
+
+    EVENT_HANDLER.with(|event_handler| (event_handler.borrow_mut())())
 }
