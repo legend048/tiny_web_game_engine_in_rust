d80431ea515ae9edb9111f9cf17184f6639ea338
legend048 <pklegend048@gmail.com>
2024-12-15 11:38:41 +0530

    rect animation

--- examples/game.rs
+++ examples/game.rs
@@ -11,25 +11,28 @@ fn main() {
     let mut x_pos = 200.0;
     let mut y_pos = 30.0;
 
-    game_engine::set_event_handler(move |key| {
-        // match key {
-        //     game_engine::Key::Left => game_engine::clear_screen_color(1.0, 0.0, 0.0, 1.0),
-        //     game_engine::Key::Right => game_engine::clear_screen_color(0.0, 1.0, 0.0, 1.0),
-        //     game_engine::Key::Up => game_engine::clear_screen_color(0.0, 0.0, 1.0, 1.0),
-        //     game_engine::Key::Down => game_engine::clear_screen_color(0.0, 1.0, 1.0, 1.0),
-        //     game_engine::Key::Space => game_engine::clear_screen_color(1.0, 1.0, 0.0, 1.0),
-        // }
-        
-        let move_amount = 20.0;
-        match key {
-            game_engine::Key::Left => x_pos -= move_amount,
-            game_engine::Key::Right => x_pos += move_amount,
-            game_engine::Key::Up => y_pos += move_amount,
-            game_engine::Key::Down => y_pos -= move_amount,
-            game_engine::Key::Space => {},
-        }
+    
+    let mut x_dir = 1.0;
+    let mut y_dir = 1.0;
 
-        game_engine::clear_screen_color(0.0, 0.0, 0.3, 1.0);
-        game_engine::draw_rectangle(x_pos, y_pos, 100., 100.);
+    let speed = 5.0;
+
+    game_engine::set_event_handler(move |event| match event {
+        game_engine::Event::Draw => {
+            x_pos += x_dir * speed;
+            y_pos += y_dir * speed;
+
+            if x_pos <= 0.0 || x_pos >= 500.0 {
+                x_dir *= -1.0;
+            }
+
+            if y_pos <= 0.0 || y_pos >= 500.0 {
+                y_dir *= -1.0;
+            }
+
+            game_engine::clear_screen_color(0.0, 0.0, 0.3, 1.0);
+            game_engine::draw_rectangle(x_pos, y_pos, 100., 100.);
+        }
+        _ => {}
     });
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,11 @@ pub enum Key {
     Space
 }
 
+pub enum Event{
+    KeyDown(Key),
+    Draw,
+}
+
 extern "C" {
     pub fn log_number(number: usize);
     fn change_screen_color(red: f32, green: f32, blue: f32, alpha: f32);
@@ -19,10 +24,10 @@ pub fn clear_screen_color(red: f32, green: f32, blue: f32, alpha: f32){
 }
 
 thread_local! {
-    pub static EVENT_HANDLER: std::cell::RefCell<Box<dyn FnMut(Key)>> = std::cell::RefCell::new(Box::new(|_|{}));
+    pub static EVENT_HANDLER: std::cell::RefCell<Box<dyn FnMut(Event)>> = std::cell::RefCell::new(Box::new(|_|{}));
 }
 
-pub fn set_event_handler(function: impl FnMut(Key) + 'static) {
+pub fn set_event_handler(function: impl FnMut(Event) + 'static) {
     EVENT_HANDLER.with(|event_handler| {
         *event_handler.borrow_mut() = Box::new(function);
     });
@@ -41,7 +46,7 @@ pub extern "C" fn key_pressed(value: usize) {
         _ => return,
     };
 
-    EVENT_HANDLER.with(|event_handler| (event_handler.borrow_mut())(key))
+    EVENT_HANDLER.with(|event_handler| (event_handler.borrow_mut())(Event::KeyDown(key)))
 }
 
 
@@ -49,4 +54,9 @@ pub fn draw_rectangle(x: f32, y: f32, width: f32, height: f32){
     unsafe {
         js_draw_rectangle(x, y, width, height);
     }
+}
+
+#[no_mangle]
+pub extern "C" fn animate(){
+    EVENT_HANDLER.with(|event_handler| (event_handler.borrow_mut())(Event::Draw))
 }
